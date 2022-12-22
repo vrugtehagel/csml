1d1ddd3534aabe335b55def9339e8512c27e35f1
vrugtehagel <vrugtehagel@gmail.com>
2022-12-22 10:04:38 +0100

    Added JSDoc strings

--- index.js
+++
@@ -1,5 +0,0 @@
-import CSML from './build/index.js'
-
-export const render = (...args) => CSML.render(...args)
-export const addTransform = (...args) => CSML.addTransform(...args)
-export const removeTransform = (...args) => CSML.removeTransform(...args)
