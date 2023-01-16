7aa2402b6e9e03e0a795133061ab3fb573ebeec1
vrugtehagel <vrugtehagel@gmail.com>
2023-01-16 23:15:40 +0100

    Update documentation further

--- mod.ts
+++ mod.ts
@@ -19,44 +19,24 @@ type Module = Record<any, any>
  */
 export interface CSML {
     /**
-     * Retrieves the arguments passed to the current module.
-     * 
-     * If there are no arguments, or if it is used outside a module, this just
-     * returns `undefined`.
+     * Retrieves the arguments passed to the current module. If there are no
+     * arguments, or if it is used outside a module, this just returns
+     * `undefined`.
      */
     readonly args: unknown
 
     /**
-     * Imports a CSML module, including the HTML and other exports.
-     * 
-     * @param url An absolute URL to a CSML module.
-     * @param args An arguments object to pass to the CSML module. The module
-     *             will be able to access this argument through `csml.args`.
-     *             While it is recommended that this is an object, it is also
-     *             possible to pass anything else here.
-     * @returns A promise resolving to a `Module` object, including all exports
-     *          from the CSML file. The HTML output is included as the default
-     *          export.
+     * Imports a CSML module, including the HTML and other, user-defined
+     * exports. Every time this is called, the module is re-run.
      */
     import(url: string | URL, args?: any): Promise<Module>
 
     /**
-     * Renders a CSML module.
-     * 
-     * @example
-     * ```js
-     * const url = new URL('./about-page.csml', import.meta.url)
-     * const html = await csml.render(url, {name: 'vrugtehagel'})
-     * 
-     * console.log(html) // "<!DOCTYPE html><html lang=en><meta charset=utf..."
-     * ```
-     * 
-     * @param url An absolute URL to a CSML module.
-     * @param args An arguments object to pass to the CSML module. The module
-     *             will be able to access this argument through `csml.args`.
-     *             While it is recommended that this is an object, it is also
-     *             possible to pass anything else here.
-     * @returns A promise resolving to the output HTML.
+     * Renders a CSML module. This is functionally equivalent to importing it
+     * using `csml.import`, and then reading the default export. You can use
+     * this to insert some HTML from another template into the current one,
+     * through `:html {{ csml.render(...) }}`. Note that interpolation does not
+     * require the `await` keyword for this.
      */
     render(url: string | URL, args?: any): Promise<string>
 
@@ -64,10 +44,12 @@ export interface CSML {
 
 /**
  * Import and render CSML modules.
+ * 
+ * For details, see the docs for the
+ * [CSML interface](https://deno.land/x/csml/mod.ts?s=CSML).
  */
-
 export const csml: CSML = {
-    args: undefined,
+    get args(){ return undefined },
 
     async import(url: string | URL, args?: any): Promise<Module> {
         return await globalCSML.import(url, args)
